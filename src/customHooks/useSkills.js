c11f8bda90a98f4f46c03f686afef74c8d3660e0
本多葵 <hondaaoi@hondaaoinoMacBook-Pro.local>
2022-03-04 01:14:35 +0900

    リファクタリング

--- src/customHooks/useSkills.js
+++ src/customHooks/useSkills.js
@@ -1,43 +1,70 @@
 import { useEffect, useReducer } from "react";
 import axios from "axios";
-import { skillReducer, initialState, actionTypes } from "../reducers/skillReducer";
+import {
+  skillReducer,
+  initialState,
+  actionTypes,
+} from "../reducers/skillReducer";
+import { requestStates } from "../constants";
 
 export const useSkills = () => {
   const [state, dispatch] = useReducer(skillReducer, initialState);
-  
+
+  const fetchReposApi = () => {
+    axios
+      .get("https://api.github.com/users/AOI0107/repos")
+      .then((response) => {
+        const languageList = response.data.map((res) => res.language);
+        const countedLanguageList = generateLanguageCountObj(languageList);
+        dispatch({
+          type: actionTypes.success,
+          payload: { languageList: countedLanguageList },
+        });
+      })
+      .catch(() => {
+        dispatch({ type: actionTypes.error });
+      });
+  };
+
+  useEffect(() => {
+    if (state.requestState !== requestStates.loading) {
+      return;
+    }
+    fetchReposApi();
+  }, [state.requestState]);
+
   useEffect(() => {
     dispatch({ type: actionTypes.fetch });
-    axios.get('https://api.github.com/users/AOI0107/repos')
-    .then((response) => {
-  const languageList = response.data.map(res => res.language);
-  const countedLanguageList = generateLanguageCountObj(languageList);
-  dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
-  })
-  .catch(() => {
-    dispatch({ type: actionTypes.error });
-  });
   }, []);
 
   const generateLanguageCountObj = (allLanguageList) => {
-    const notNullLanguageList = allLanguageList.filter(language => language != null);
+    const notNullLanguageList = allLanguageList.filter(
+      (language) => language != null
+    );
     const uniqueLanguageList = [...new Set(notNullLanguageList)];
 
-    return uniqueLanguageList.map(item => {
+    return uniqueLanguageList.map((item) => {
       return {
         language: item,
-        count: allLanguageList.filter(language => language === item).length
-      }
+        count: allLanguageList.filter((language) => language === item).length,
+      };
     });
   };
 
-  const converseCountToPercentage = (count) => {
-    if (count > 10) { return 100; }
-    return count * 10;
+  const DEFAULT_MAX_PERCENTAGE = 100;
+  const LANGUAGE_COUNT_BASE = 10;
+
+  const converseCountToPercentage = (languageCount) => {
+    if (languageCount > LANGUAGE_COUNT_BASE) {
+      return DEFAULT_MAX_PERCENTAGE;
+    }
+    return languageCount * LANGUAGE_COUNT_BASE;
   };
 
-  const sortedLanguageList = () => (
-    state.languageList.sort((firstLang, nextLang) => nextLang.count - firstLang.count)
-  )
+  const sortedLanguageList = () =>
+    state.languageList.sort(
+      (firstLang, nextLang) => nextLang.count - firstLang.count
+    );
 
   return [sortedLanguageList, state.requestState, converseCountToPercentage];
-}
+};
