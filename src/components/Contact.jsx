bfb07e8513cdd2f5e6830686361dfd3b306cf591
本多葵 <hondaaoi@hondaaoinoMacBook-Pro.local>
2022-03-03 23:45:42 +0900

    4-6

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+export const Contact = () => {
+  return (
+    <div id="contact" className="contact">
+      <div className="container">
+        <div className="contact-contents">
+          <h2>Contact to Me!</h2>
+          <p className="description">ご連絡はこちらまで</p>
+          <a href="https://twitter.com/aomomo123" className="btn">Contact Us</a>
+        </div>
+      </div>
+    </div>
+  );
+};
